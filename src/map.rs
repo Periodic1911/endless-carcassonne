f9e8aa74d8a934a0206eb3870079f65cf9639f73
Anne Stijns <anstijns@gmail.com>
2024-06-04 17:24:37 +0200

    just keep the map fixed for now, use a trait

--- src/map.rs
+++ src/map.rs
@@ -1,74 +1,45 @@
-/** Expandable 2d map.
-* 
-* Can expand from the origin (0, 0) in both positive
-* and negative direction.
+/** 2D map trait
+* Anything that can be indexed by a 2D point can implement this trait
 */
-pub struct Map2D<T> where T: Default + Clone {
-    up: Vec<[Vec<T>;2]>,
-    down: Vec<[Vec<T>;2]>,
+pub trait Map2D<T> where T: Default {
+    fn new(xmin: isize, ymin: isize, width: usize, height: usize) -> Self;
+    fn get(&self, x: isize, y: isize) -> &T;
+    fn get_mut(&mut self, x: isize, y: isize) -> &mut T;
 }
 
-impl<T> Map2D<T> where T: Default + Clone {
-    /// Create new map filled with the default entry
-    /// xmin is how far you can index in the negative axis, xmax is how far in the positive.
-    /// For example: `Map2D::new(10, 10, 0, 5)` allows indexing from -10 to 10 in the x axis
-    /// (inclusive) and from (0, 10) in the y axis (also inclusive).
-    pub fn new(xmin: usize, xmax: usize, ymin: usize, ymax: usize) -> Self {
+struct FixedMap<T> where T: Default + Clone {
+    vec: Vec<T>,
+    width: usize,
+    height: usize,
+    xmin: isize,
+    ymin: isize,
+}
+
+impl<T> Map2D<T> for FixedMap<T> where T: Default + Clone {
+    fn new(xmin: isize, ymin: isize, width: usize, height: usize) -> Self {
         Self {
-            up: vec![[vec![T::default(); xmin], vec![T::default(); xmax+1]]; ymax+1],
-            down: vec![[vec![T::default(); xmin], vec![T::default(); xmax+1]]; ymin],
+            vec: vec![T::default(); width*height],
+            width,
+            height,
+            xmin,
+            ymin
         }
     }
-    
-    /// Get an array at index (x, y)
-    /// Panics if the point is out-of-bounds
-    pub fn get(&self, x: isize, y: isize) -> &T {
-        let y_vec = if y < 0 {
-            &self.down
-        } else {
-            &self.up
-        };
 
-        let y: usize = Self::signed_index(y);
-
-        let x_vec = if x < 0 {
-            &y_vec[y][0]
-        } else {
-            &y_vec[y][1]
-        };
-
-        let x: usize = Self::signed_index(x);
-
-        &x_vec[x]
-    }
-    
-    pub fn get_mut(&mut self, x: isize, y: isize) -> &mut T {
-        let y_vec = if y < 0 {
-            &self.down
-        } else {
-            &self.up
-        };
-
-        let y: usize = Self::signed_index(y);
-
-        let mut x_vec = if x < 0 {
-            &mut y_vec[y][0]
-        } else {
-            &mut y_vec[y][1]
-        };
-
-        let x: usize = Self::signed_index(x);
-
-        &mut x_vec[x]
+    fn get(&self, x: isize, y: isize) -> &T {
+        let x = (x-self.xmin) as usize;
+        let y = (y-self.ymin) as usize;
+        debug_assert!(x < self.width);
+        debug_assert!(y < self.height);
+        &self.vec[ x + self.width * y ]
     }
 
-    fn signed_index(x: isize) -> usize {
-        return if x < 0 {
-            -(x + 1)
-        } else {
-            x
-        }.try_into().unwrap();
-
+    fn get_mut(&mut self, x: isize, y: isize) -> &mut T {
+        let x = (x-self.xmin) as usize;
+        let y = (y-self.ymin) as usize;
+        debug_assert!(x < self.width);
+        debug_assert!(y < self.height);
+        &mut self.vec[ x + self.width * y ]
     }
 }
 
@@ -78,23 +49,52 @@ mod tests {
 
     #[test]
     fn new() {
-        let _map: Map2D<u8> = Map2D::new(10, 10, 10, 10);
+        let _map: FixedMap<u8> = FixedMap::new(-10, -10, 20, 20);
     }
 
     #[test]
     fn get() {
-        let map: Map2D<u8> = Map2D::new(10, 10, 10, 10);
+        let map: FixedMap<u8> = FixedMap::new(-10, -10, 20, 20);
         assert_eq!(map.get(0,0), &u8::default());
-        assert_eq!(map.get(10,0), &u8::default());
+    }
+
+    #[test]
+    fn get1() {
+        let map: FixedMap<u8> = FixedMap::new(-10, -10, 20, 20);
+        assert_eq!(map.get(9,0), &u8::default());
+    }
+    #[test]
+    fn get2() {
+        let map: FixedMap<u8> = FixedMap::new(-10, -10, 20, 20);
         assert_eq!(map.get(-10,0), &u8::default());
-        assert_eq!(map.get(0,10), &u8::default());
+    }
+    #[test]
+    fn get3() {
+        let map: FixedMap<u8> = FixedMap::new(-10, -10, 20, 20);
+        assert_eq!(map.get(0,9), &u8::default());
+    }
+    #[test]
+    fn get4() {
+        let map: FixedMap<u8> = FixedMap::new(-10, -10, 20, 20);
         assert_eq!(map.get(0,-10), &u8::default());
     }
 
     #[test]
     #[should_panic]
     fn get_out_of_bounds() {
-        let map: Map2D<u8> = Map2D::new(10, 10, 10, 10);
-        map.get(11,0);
+        let map: FixedMap<u8> = FixedMap::new(-10, -10, 20, 20);
+        map.get(10,0);
+    }
+
+    #[test]
+    fn get_mut() {
+        let mut map: FixedMap<u8> = FixedMap::new(-10, -10, 20, 20);
+        for i in -10..10 {
+            for j in -10..10 {
+                let trial_number = (i+j*20) as u8;
+                *map.get_mut(i,j) = trial_number;
+                assert_eq!(map.get(i,j), &trial_number);
+            }
+        }
     }
 }
